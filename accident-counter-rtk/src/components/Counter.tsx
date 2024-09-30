822e0b2c7c61df2cbf06877b3e7d7c8636d1dca0
Andy Swanepoel <andyp.swanepoel@gmail.com>
2024-09-30 08:27:19 -0400

    reducer example

--- accident-counter-rtk/src/components/Counter.tsx
+++ accident-counter-rtk/src/components/Counter.tsx
@@ -1,16 +1,42 @@
+import * as React from 'react';
+import {
+  counterReducer,
+  decrement,
+  increment,
+  reset,
+} from '../features/counterReducer';
+
 const Counter = () => {
+  const [value, setValue] = React.useState(0);
+
+  // Here, we're using a RTK reducer in a React useReducer
+  const [counterState, dispatch] = React.useReducer(counterReducer, {
+    count: 0,
+  });
   return (
-    <section className="flex flex-col items-center w-2/3 gap-8 p-8 bg-white border-4 shadow-lg border-primary-500">
+    <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">0</p>
+      <p className="text-6xl">{counterState.count}</p>
       <div className="flex gap-2">
-        <button>➖ Decrement</button>
-        <button>🔁 Reset</button>
-        <button>➕ Increment</button>
+        <button onClick={() => dispatch(decrement(1))}>➖ Decrement</button>
+        <button onClick={() => dispatch(reset())}>🔁 Reset</button>
+        <button onClick={() => dispatch(increment(1))}>➕ Increment</button>
       </div>
       <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input type="number" value={0} onChange={() => {}} />
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(increment(value));
+            setValue(0);
+          }}
+        >
+          <input
+            type="number"
+            value={value}
+            onChange={(e) => {
+              setValue(parseInt(e.target.value, 10));
+            }}
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
