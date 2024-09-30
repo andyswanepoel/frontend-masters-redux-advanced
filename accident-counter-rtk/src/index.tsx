ed922f28b31b47456afc5f30b913f2a4b60b6a50
Andy Swanepoel <andyp.swanepoel@gmail.com>
2024-09-29 20:43:25 -0400

    rename and update packages

--- accident-counter-rtk/src/index.tsx
+++ accident-counter-rtk/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Application from './components/application';
+import Application from './components/Application';
 
 import './index.css';
 
